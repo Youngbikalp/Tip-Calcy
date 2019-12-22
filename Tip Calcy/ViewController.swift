e547a2d80ede01519985f12e090a6dd29e683f3e
Bikalp Timalsina <bikalp16@gmail.com>
2019-12-22 15:22:58 -0800

    Initial Commit

---
+++ Tip Calcy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calcy
+//
+//  Created by Bikalp  Timalsina on 12/22/19.
+//  Copyright © 2019 Bikalp  Timalsina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
